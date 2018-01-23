aa31e60c3c19aded9b9e6de02b5b1800e93f4b13
PJ Frias <peejfrias@gmail.com>
2018-01-23 02:03:46 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE series (
   author_id INTEGER,
   subgenre_id INTEGER
 );
+
+CREATE TABLE subgenre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE author (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
