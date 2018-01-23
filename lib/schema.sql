d6f155afadfc91a520ff16e138f1c9eca42781a2
PJ Frias <peejfrias@gmail.com>
2018-01-23 02:05:15 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -19,3 +19,12 @@ CREATE TABLE book (
   title TEXT,
   year INTEGER
 );
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
